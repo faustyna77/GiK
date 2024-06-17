f0f0fe61d661eb0f50fc4c867c42338acc89e747
Faustyna Misiura <110495453+faustyna77@users.noreply.github.com>
2024-06-17 21:58:01 +0200

    Create App.java

---
+++ gik_solut/App.java
@@ -0,0 +1,29 @@
+package GiKCzK.lab;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+
+public class App {
+    String version = "0.02";
+
+    public static void main(String[] args) {
+
+        Renderer mainRenderer = new Renderer(System.getProperty("user.home")+"/render.png");
+        mainRenderer.clear();
+        mainRenderer.drawPoint(100, 100);
+        try {
+            mainRenderer.save();
+        } catch (IOException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public String getVersion() {
+        return this.version;
+    }
+}
